61923e050235a52a1cc503ad67d961a53a0cb2dc
mirko-console-dir <cdmirko.saponaro@gmail.com>
2025-05-20 00:09:34 +0900

    fix origin for the client port

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   // here all the origins that can fetch our server (rest api) at localhost:3500
-  "http://localhost:3000",
+  "http://localhost:5173",
 ];
 
 module.exports = allowedOrigins;
